26208a96c4c8359400a68f5cc0f78c47f8832969
Nick Rodriguez <endotnick@gmail.com>
2017-08-23 18:57:45 +0000

    add dungeon

---
+++ dungeon.rb
@@ -0,0 +1,33 @@
+class Dungeon
+    attr_accessor :player
+    
+    def initialize(player_name)
+        @player = Player.new(player_name)
+        @rooms = []
+    end
+    
+    Player = Struct.new(:name, :location)
+    Room = Struct.new(:reference, :name, :description, :connections)
+end
+    
+=begin   
+    class Player
+       attr_accessor :name, :location
+       
+       def initialize(player_name)
+           @name = player_name
+       end
+    end
+    
+    class Room
+        attr_accessor :reference, :name, :description, :connections
+        
+        def initialize(reference, name, description, connections)
+            @reference = reference
+            @name = name
+            @description = description
+            @connections = connections
+        end
+    end
+end
+=end
